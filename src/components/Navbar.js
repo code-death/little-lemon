acf24be996037201f47efbe96a7f974f6edeaf83
code-death <poorvanshkavta938@gmail.com>
2023-04-22 23:08:18 +0530

    Added Navbar Sestion

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <img src="" alt="little lemon logo" />
+        <ul className='sectionLinks'>
+            <li className='sectionLink'><a href="#home">Home</a></li>
+            <li className='sectionLink'><a href="#home">About</a></li>
+            <li className='sectionLink'><a href="#home">Menu</a></li>
+            <li className='sectionLink'><a href="#home">Reservation</a></li>
+            <li className='sectionLink'><a href="#home">Order Online</a></li>
+            <li className='sectionLink'><a href="#home">Login</a></li>
+        </ul>
+    </nav>
+  )
+}
