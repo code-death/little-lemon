0a1e4389883938fb577cd2a0e1897340901347e2
code-death <poorvanshkavta938@gmail.com>
2023-04-22 23:12:33 +0530

    Added hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='hero'>
+        <div className='heroText'>
+        <h1>Little Lemon</h1>
+        <h3>Chicago</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <button><a href="/reservation">Reserve Table</a></button>
+        </div>
+        <img src="" alt="" />
+    </div>
+  )
+}
