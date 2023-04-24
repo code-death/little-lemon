960e20fc674345bb9a2f4d7017eec0f7d1b55d9f
code-death <poorvanshkavta938@gmail.com>
2023-04-24 13:13:25 +0530

    Testimonials done

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,17 +1,43 @@
 import React from 'react'
 
+const users = [
+  {
+    img: '',
+    name: 'Adam',
+    rating: 4.5,
+  },
+  {
+    img: '',
+    name: 'Martha',
+    rating: 4,
+  },
+  {
+    img: '',
+    name: 'William',
+    rating: 4.5,
+  },
+  {
+    img: '',
+    name: 'Maria',
+    rating: 5,
+  },
+]
+
+
 export default function Testimonials() {
   return (
     <div className='testimonials'>
         <h1 className='testimonialHeading'>Testimonials</h1>
-        <div className='testimonialUser'>
-            <img src="" alt="UserImage" />
-            <div className='userDetails'>
-                <h3 className='username'>Username</h3>
-                <p className='rating'>Rating</p>
-            </div>
-            <p className='userTestimonial'></p>
-        </div>
+        {users.map(user => (
+          <div key={user.name} className='testimonialUser'>
+          <img src={user.img} alt="UserImage" />
+          <div className='userDetails'>
+              <h3 className='username'>{user.name}</h3>
+              <p className='rating'>{user.rating}</p>
+          </div>
+          <p className='userTestimonial'></p>
+      </div>
+        ))}
     </div>
   )
 }
